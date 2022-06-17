b412f5dab5c728163da80eeac714edb30a9dd5e6
mohammad sadegh masoumi <sadegh.masoumi.ms@gmail.com>
2022-06-17 17:46:31 +0430

    start project

---
+++ README.md
@@ -0,0 +1,19 @@
+# Chat-Server 🐍 : A Python-Based Chat Room
+
+This is an advanced Python-based Secure Chat room. The project is entirely based on the Socket Progamming; done using Python. A server is set to the listening mode, with a specific IP Address and Port numner (that can be edited in the script) and clients are made to connect to the server, after which they are promopted to enter a nickname. The messages are then broadcasted to all the clients present. 
+
+
+## 👉 Usage
+
+1. We will have to start our Server first.
+``` shell
+Python Server.py
+```
+<b>Note: </b> Before running the server, make sure to edit the IP address and Port number. By default it is running on Localhost:5555 <br><br>
+2. Run the Client file, to start the conversation. 
+``` Shell
+Python Client.py
+```
+<br>
+3. Now Enter a nickname and start your chatting. 
+
---
+++ client.py
@@ -0,0 +1,65 @@
+import socket
+import threading
+
+nickname = input("Choose Your Nickname:")
+if nickname == 'admin':
+    password = input("Enter Password for Admin:")
+    
+print(chr(27) + "[2J")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Connect to a host
+client.connect(('127.0.0.1',5555))
+
+stop_thread = False
+
+def recieve():
+    while True:
+        global stop_thread
+        if stop_thread:
+            break    
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+                next_message = client.recv(1024).decode('ascii')
+                if next_message == 'PASS':
+                    client.send(password.encode('ascii'))
+                    if client.recv(1024).decode('ascii') == 'REFUSE':
+                        print("Connection is Refused !! Wrong Password")
+                        stop_thread = True
+                # Clients those are banned can't reconnect
+                elif next_message == 'BAN':
+                    print('Connection Refused due to Ban')
+                    client.close()
+                    stop_thread = True
+            else:
+                print(message)
+        except:
+            print('Error Occured while Connecting')
+            client.close()
+            break
+        
+def write():
+    while True:
+        if stop_thread:
+            break
+        #Getting Messages
+        message = f'{nickname}: {input("")}'
+        if message[len(nickname)+2:].startswith('/'):
+            if nickname == 'admin':
+                if message[len(nickname)+2:].startswith('/kick'):
+                    # 2 for : and whitespace and 6 for /KICK_
+                    client.send(f'KICK {message[len(nickname)+2+6:]}'.encode('ascii'))
+                elif message[len(nickname)+2:].startswith('/ban'):
+                    # 2 for : and whitespace and 5 for /BAN
+                    client.send(f'BAN {message[len(nickname)+2+5:]}'.encode('ascii'))
+            else:
+                print("Commands can be executed by Admins only !!")
+        else:
+            client.send(message.encode('ascii'))
+
+recieve_thread = threading.Thread(target=recieve)
+recieve_thread.start()
+write_thread = threading.Thread(target=write)
+write_thread.start()
